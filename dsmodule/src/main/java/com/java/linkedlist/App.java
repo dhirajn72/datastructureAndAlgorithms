7275b8386308af4ec1a028109a9c20c6663ba6d4
Dhiraj Kumar <dhiraj.kumar1@myntra.com>
2018-02-28 18:08:41 +0530

    [DS] : Program to find middle element of linked list

---
+++ dsmodule/src/main/java/com/java/linkedlist/App.java
@@ -0,0 +1,13 @@
+package com.java.linkedlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
