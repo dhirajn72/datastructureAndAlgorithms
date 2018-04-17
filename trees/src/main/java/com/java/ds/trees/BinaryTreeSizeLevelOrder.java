3e3a3e41b7a9ffae2b5b31b1db71d96322cb4ba8
DhirajSingh <dhirajn72@users.noreply.github.com>
2018-04-17 10:27:13 +0530

    Update BinaryTreeSizeLevelOrder.java

--- trees/src/main/java/com/java/ds/trees/BinaryTreeSizeLevelOrder.java
+++ trees/src/main/java/com/java/ds/trees/BinaryTreeSizeLevelOrder.java
@@ -1,14 +1,13 @@
-/*Copyright (c) Dec 21, 2014 CareerMonk Publications and others.
- * E-Mail           	: info@careermonk.com
- * Creation Date    	: 2015-01-10 06:15:46
- * Last modification	: 2006-05-31
-               by		: Narasimha Karumanchi
- * File Name			: BinaryTreeSizeLevelOrder.java
- * Book Title			: Data Structures And Algorithms Made In Java
- * Warranty         	: This software is provided "as is" without any
- * 							warranty; without even the implied warranty of
- * 							merchantability or fitness for a particular purpose.
- *
+/*Copyright (c) April 10, 2018 @DhirajSingh.
+ * E-Mail           	: dhirajn72@gmail.com
+ * Creation Date    	: 2018-04-10 03:15:46
+ * Last modification	: 2018-04-10
+               by		: Dhiraj Singh
+ * Book Title			: Data Structures And Algorithms
+ * Warranty         	: This software is provided "as is" without any 
+ * 							warranty; without even the implied warranty of 
+ * 							merchantability or fitness for a particular purpose. 
+ * 
  */
 
 
